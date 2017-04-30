bf6f7dfaa258e8721d7146d6396f1b50a5e88ee0
Maranatha A. Ilesanmi <maranatha.ilesanmi@andela.com>
2017-04-30 19:20:10 +0100

    [144587849] add sharing feature to articles.

---
+++ app/share/Share.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { ShareButtons, generateShareIcon } from 'react-share';
+
+const {
+  FacebookShareButton,
+  GooglePlusShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+} = ShareButtons;
+
+const FacebookIcon = generateShareIcon('facebook');
+const TwitterIcon = generateShareIcon('twitter');
+const GooglePlusIcon = generateShareIcon('google');
+const LinkedinIcon = generateShareIcon('linkedin');
+const WhatsappIcon = generateShareIcon('whatsapp');
+
+class Share extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const shareUrl = this.props.share;
+    const title = this.props.title;
+    const divStyle = {
+      position: 'absolute',
+      right: '24px',
+    };
+
+    return (
+      <div>
+        <div className="fixed-action-btn" style={divStyle}>
+          <a className="btn-floating btn-large red">
+            <span>Share via</span>
+            <i className="large material-icons">loyalty</i>
+          </a>
+          <ul>
+            <li>
+              <FacebookShareButton url={shareUrl} title={title}>
+                <FacebookIcon size={32} round />
+              </FacebookShareButton>
+            </li>
+            <li>
+              <TwitterShareButton url={shareUrl} title={title}>
+                <TwitterIcon size={32} round />
+              </TwitterShareButton>
+            </li>
+            <li>
+              <GooglePlusShareButton url={shareUrl}>
+                <GooglePlusIcon size={32} round />
+              </GooglePlusShareButton>
+            </li>
+            <li>
+              <LinkedinShareButton url={shareUrl} title={title}>
+                <LinkedinIcon size={32} round />
+              </LinkedinShareButton>
+            </li>
+            <li>
+              <WhatsappShareButton url={shareUrl} title={title}>
+                <WhatsappIcon size={32} round />
+              </WhatsappShareButton>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Share;
--- app/test/actions/newsAction.test.jsx
+++ app/test/actions/newsAction.test.jsx
@@ -10,3 +10,29 @@
 //     expect(typeof SourcesStore.sources).toBe('object');
 //   });
 // });
+
+import mockedAxios from './apiMockCall';
+import NewsActionTypes from '../../constants/NewsFeedConstants';
+import NewsDispatcher from '../../dispatcher/AppDispatcher';
+import NewsActions from '../../actions/newsAction';
+
+describe('#getNews() using Promises', () => {
+  let testAction;
+  beforeEach(() => {
+    jest.mock('axios', () => mockedAxios);
+    testAction = jest.spyOn(NewsDispatcher, 'dispatch');
+  });
+
+  afterEach(() => {
+    testAction.mockReset();
+  });
+
+  it('should load news data', () => NewsActions.getNews('bbcnews')
+    .then(() => {
+      const arg = testAction.mock.calls[0][0];
+      expect(testAction).toHaveBeenCalled();
+      expect(arg.eventName).toEqual(NewsActionTypes.GET_NEWS);
+      expect(arg.news).toBeInstanceOf(Object);
+      expect(arg.news[0].meta).toEqual('BBC News');
+    }));
+});
