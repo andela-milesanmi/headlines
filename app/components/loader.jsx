179173a3674c269f3ac6acad35c8ebcfdcaaf349
Maranatha A. Ilesanmi <maranatha.ilesanmi@andela.com>
2017-05-06 22:30:45 +0100

    [144714671] convert empty tags to self-closing tags

--- app/components/loader.jsx
+++ app/components/loader.jsx
@@ -11,13 +11,13 @@ const Loading = () => (
       <div className="preloader-wrapper big active">
         <div className="spinner-layer spinner-green-only">
           <div className="circle-clipper left">
-            <div className="circle"></div>
+            <div className="circle" />
           </div>
           <div className="gap-patch">
-            <div className="circle"></div>
+            <div className="circle" />
           </div>
           <div className="circle-clipper right">
-            <div className="circle"></div>
+            <div className="circle" />
           </div>
         </div>
       </div>
