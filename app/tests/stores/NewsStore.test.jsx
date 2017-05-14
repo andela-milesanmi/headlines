0a76c3ee86bb17196dd1469109dab58a9d33f092
Maranatha A. Ilesanmi <maranatha.ilesanmi@andela.com>
2017-05-14 08:44:22 +0100

    update store test file

--- app/tests/stores/NewsStore.test.jsx
+++ app/tests/stores/NewsStore.test.jsx
@@ -1,5 +1,5 @@
 import expect from 'expect';
-import NewsStore from '../../stores/NewsStore';
+import NewsStore from '../../stores/NewsStore.js';
 import Dispatcher from '../../dispatcher/AppDispatcher';
 import ActionTypes from '../../constants/ActionTypes.jsx';
 import { fetchedArticles } from '../testData';
