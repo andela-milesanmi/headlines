baef3ac3286eac9c7e743ae39eeeda4a2817981d
Maranatha A. Ilesanmi <maranatha.ilesanmi@andela.com>
2017-05-14 01:31:59 +0100

    update the storenames in the test files

--- app/tests/stores/SourcesStore.test.jsx
+++ app/tests/stores/SourcesStore.test.jsx
@@ -1,5 +1,5 @@
 import expect from 'expect';
-import SourcesStore from '../../stores/SourcesStore';
+import SourcesStore from '../../stores/SourcesStore.jsx';
 import Dispatcher from '../../dispatcher/AppDispatcher';
 import ActionTypes from '../../constants/ActionTypes.jsx';
 import { fetchedSources } from '../testData';
